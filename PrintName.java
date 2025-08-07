704be35cc1c44be624d4fb2e7c0a881e912686a0
Manuradha Singh <155948399+manuradhagla@users.noreply.github.com>
2025-08-07 15:19:03 +0530

    new code

---
+++ PrintName.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PrintName {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name=sc.next();
+        System.out.println("Wlcome to BridgeLabz! "+name);
+        sc.close();
+    }
+}
